340f15f799701fdff0eff5d5e148cfc1c573e4f7
Huy Nguyen <huyy.802@gmail.com>
2024-04-08 23:35:52 +0700

    feat: fetch weather data using current location

--- src/components/form/SearchForm/index.js
+++ src/components/form/SearchForm/index.js
@@ -24,43 +24,66 @@ function SearchForm() {
     dispatch(fetchWeather(cityName));
   };
 
+  const handleUseCurrentLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          console.log("Latitude:", latitude);
+          console.log("Longitude:", longitude);
+          dispatch(fetchWeather(`${latitude},${longitude}`));
+        },
+        (error) => {
+          console.error("Error getting current location:", error.message);
+          alert("Error getting current location. Please try again later.");
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by your browser.");
+    }
+  };
+
   return (
-    <form onSubmit={handleFormSubmit} className="search-form">
-      <div className="form-row">
-        <label htmlFor="cityInput" className="label-text">
-          Enter a City Name
-        </label>
-      </div>
-      <div className="form-row">
-        <input
-          type="text"
-          id="cityInput"
-          placeholder="E.g., New York, London, Tokyo"
-          value={cityName}
-          onChange={handleInputChange}
-          className="input-field"
-        />
-      </div>
-      <div className="form-row">
-        <Button
-          label="Search"
-          color={colors.cornflowerBlue}
-          height="50px"
-          width="370px"
-          onClick={handleFormSubmit}
-        />
-      </div>
-      {/* <TextLine text="or" /> */}
+    <div>
+      <form onSubmit={handleFormSubmit} className="search-form">
+        <div className="form-row">
+          <label htmlFor="cityInput" className="label-text">
+            Enter a City Name
+          </label>
+        </div>
+        <div className="form-row">
+          <input
+            type="text"
+            id="cityInput"
+            placeholder="E.g., New York, London, Tokyo"
+            value={cityName}
+            onChange={handleInputChange}
+            className="input-field"
+          />
+        </div>
+        <div className="form-row">
+          <Button
+            type="button"
+            label="Search"
+            color={colors.cornflowerBlue}
+            height="50px"
+            width="370px"
+            onClick={() => handleFormSubmit()}
+          />
+        </div>
+        {/* <TextLine text="or" /> */}
+      </form>
       <div className="form-row">
         <Button
           label="Use Current Location"
           color={colors.slateGrey}
           height="50px"
           width="370px"
+          onClick={handleUseCurrentLocation}
         />
       </div>
       {loading && <div>Loading...</div>}
-    </form>
+    </div>
   );
 }
 
