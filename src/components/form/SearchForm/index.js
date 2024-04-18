c23651cf9160a2f518fb9903402d16bb1d86aba2
Huy Nguyen <huyy.802@gmail.com>
2024-04-18 22:19:07 +0700

    chore: remove redundance code

--- src/components/form/SearchForm/index.js
+++ src/components/form/SearchForm/index.js
@@ -68,7 +68,7 @@ function SearchForm() {
             color={colors.cornflowerBlue}
             height="50px"
             width="370px"
-            onClick={(event) => handleFormSubmit(event)}
+            onClick={() => handleFormSubmit()}
           />
         </div>
         {/* <TextLine text="or" /> */}
